20a280df0227144478ba76557b83948b5da6e874
Kartik Verma <kartikverma559@gmail.com>
2026-01-14 13:20:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int catchThieves(char[] arr, int k) {
+        // code here
+        int n=arr.length;
+        int police=0;
+        int thief=0;
+        int count=0;
+        while(police<n && arr[police]!='P') police++;
+        while(thief<n && arr[thief]!='T') thief++;
+        while(police<n && thief<n)
+        {
+            if(Math.abs(police-thief)<=k) 
+            {
+                count++;
+                police++;
+                thief++;
+            }
+            else if(police<thief && police<n ) police++;
+            else if(thief<police && thief<n) thief++;
+            while(police<n && arr[police]!='P') police++;
+            while(thief<n && arr[thief]!='T') thief++;
+        }
+        return count;
+    }
+}
