d3f18f006267394ec4ea0ea8162344d791487a4b
Kartik Verma <kartikverma559@gmail.com>
2026-02-18 23:15:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.java
@@ -0,0 +1,29 @@
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}*/
+
+class Solution {
+    Node intersectPoint(Node head1, Node head2) {
+        // code here
+        Node t1=head1;
+        Node t2=head2;
+        while(t1!=t2)
+        {
+            t1=t1.next;
+            t2=t2.next;
+            
+            if(t1==t2) return t1;
+            if(t1==null) t1=head2;
+            if(t2==null) t2=head1;
+        }
+        return t1;
+    }
+}
