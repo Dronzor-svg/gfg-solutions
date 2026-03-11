28b51a4cf442914621cccf9b69bd4cebc3de8853
Kartik Verma <kartikverma559@gmail.com>
2026-03-11 10:22:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum of subarray minimums/sum-of-subarray-minimums.java
+++ Difficulty: Medium/Sum of subarray minimums/sum-of-subarray-minimums.java
@@ -1,48 +1,83 @@
 class Solution {
     public int sumSubMins(int[] arr) {
+        // code here
+        
         int n = arr.length;
-        int[] nse = findNSE(arr);
-        int[] pse = findPSE(arr);
+        long sum = 0;
 
-        int mod = 1_000_000_007;
-        long total = 0;
-
-        for (int i = 0; i < n; i++) {
-            long left = i - pse[i];
-            long right = nse[i] - i;
-            total = (total + (left * right % mod) * arr[i]) % mod;
-        }
-
-        return (int) total;
-    }
-
-    int[] findNSE(int[] arr) {
-        int n = arr.length;
-        int[] nse = new int[n];
         Stack<Integer> st = new Stack<>();
+        int[] left = new int[n];
+        int[] right = new int[n];
 
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+        for(int i = 0; i < n; i++) {
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
                 st.pop();
             }
-            nse[i] = st.isEmpty() ? n : st.peek();
+
+            if(st.isEmpty())
+                left[i] = i + 1;
+            else
+                left[i] = i - st.peek();
+
             st.push(i);
         }
-        return nse;
-    }
 
-    int[] findPSE(int[] arr) {
-        int n = arr.length;
-        int[] pse = new int[n];
-        Stack<Integer> st = new Stack<>();
+        st.clear();
 
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) {
                 st.pop();
             }
-            pse[i] = st.isEmpty() ? -1 : st.peek();
+
+            if(st.isEmpty())
+                right[i] = n - i;
+            else
+                right[i] = st.peek() - i;
+
             st.push(i);
         }
-        return pse;
+
+        for(int i = 0; i < n; i++) {
+            sum += (long)arr[i] * left[i] * right[i];
+        }
+
+        return (int)sum;
     }
+    
 }
+
+
+
+
+
+        // int r=1;
+        // int l=0;
+        // int len=arr.length;
+        // int min=Integer.MAX_VALUE;
+        
+        // int sum=0;
+        // for(int i=0;i<len;i++)
+        // {
+        //     sum+=arr[i];
+        // }
+        // while(r<len)
+        // {
+        //     if(arr[l]<arr[r])
+        //     {
+        //         min=Math.min(min,arr[l]);
+        //         sum+=min;
+        //     }
+        //     else
+        //     {
+        //         min=Math.min(min,arr[r]);
+        //         sum+=min;
+        //     }
+        //     r++;
+        //     if(r==len)
+        //     {
+        //         l++;
+        //         r=l+1;
+        //         min=Integer.MAX_VALUE;
+        //     }
+        // }
+        // return sum;
