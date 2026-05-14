f5edc61f7c63d5f119ec4911e7b79e6b7836061e
Kartik Verma <kartikverma559@gmail.com>
2026-05-14 21:08:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search for Subarray/search-for-subarray.java
@@ -0,0 +1,70 @@
+class Solution {
+
+    public ArrayList<Integer> search(int[] a, int[] b) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int n = a.length;
+        int m = b.length;
+
+        int[] lps = buildLPS(b);
+
+        int i = 0;
+        int j = 0;
+
+        while(i < n) {
+
+            if(a[i] == b[j]) {
+                i++;
+                j++;
+            }
+
+            if(j == m) {
+                list.add(i - j);
+                j = lps[j - 1];
+            }
+            else if(i < n && a[i] != b[j]) {
+
+                if(j != 0) {
+                    j = lps[j - 1];
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+
+        return list;
+    }
+
+    static int[] buildLPS(int[] b) {
+
+        int m = b.length;
+
+        int[] lps = new int[m];
+
+        int len = 0;
+        int i = 1;
+
+        while(i < m) {
+
+            if(b[i] == b[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+
+                if(len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
+}
