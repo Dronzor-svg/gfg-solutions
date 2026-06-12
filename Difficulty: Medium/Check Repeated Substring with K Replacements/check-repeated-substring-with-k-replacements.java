a6bf4640a902d9361ab9cd9939692c34762d0627
Kartik Verma <kartikverma559@gmail.com>
2026-06-12 11:18:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Repeated Substring with K Replacements/check-repeated-substring-with-k-replacements.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean kSubstr(String s, int k) {
+        // code here
+int n = s.length();
+		
+		if (n%k != 0)
+			return false;
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		int i = 0;
+		
+		while (i<n) {
+			String temp = s.substring(i, i + k);
+			map.put(temp, map.getOrDefault(temp, 0) + 1);
+			i += k;
+		}
+		
+		int size = map.size();
+		
+		if (size == 1)
+			return true;
+		if ((size == 2) && map.containsValue(1))
+			return true;
+		
+		return false;
+	}
+}
